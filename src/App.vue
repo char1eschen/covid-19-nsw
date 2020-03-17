df29409d8cff77b14f45c21d355c693ae3ca0139
Charles Chen <martian2005@gmail.com>
2020-03-18 01:38:56 +1100

    [fix] fix agegroup chart loading issue

--- src/App.vue
+++ src/App.vue
@@ -346,7 +346,7 @@ export default {
       return (
         this.summaryDataLoading ||
         this.tableDataLoading ||
-        this.ageGroupLoading ||
+        this.agegroupLoading ||
         this.sourceLoading
       );
     }
